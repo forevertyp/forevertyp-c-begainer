07eaef471f6a9c8517b90c16d9993da9a42f5cb5
forevertyp <icetangyuping@outlook.com>
2026-01-29 00:14:07 +0800

    Refactor Record structure and function declarations

--- record.h
+++ record.h
@@ -1,32 +1,35 @@
-#ifndef RECORD_H 
+#ifndef RECORD_H
 #define RECORD_H
+
 #endif
 
-#include<stdio.h>     //printf() scanf()
-#include<unistd.h>   //sleep()
-#include<time.h>	//time() ctime()
-#include<string.h>  //strcmp() 
-#include<stdlib.h> //用于引入malloc()函数 
+#include<stdio.h>
+#include<time.h>
+#include<unistd.h>
+#include<string.h> 
+#include<stdlib.h>
 
-//用于记录学生成绩的结构体 
+//定义一个用于记录的结构体
+//包含姓名，分数，实际，
+//用链表来实现 
+
 struct Record{
-	int score;  //成绩 
-	char name[20];  //姓名 
-	time_t time;  //时间戳 
-	struct Record *next;  //链表指针域 
-}; 
+	char name[20];
+	int score;
+	time_t time;
+	struct Record *next;
+};
 
-struct Record *add_one_node(struct Record *head);//添加一个节点头插 
-struct Record *get_info(struct Record *head);   //录入信息函数头 
-struct Record *delete_node(struct Record *head);//删除一个对应学员 
-struct Record *delete_info(struct Record *head);//删除学员 
-void save_to_file(struct Record *head);  //将信息保存到文件函数头 
-struct Record* load_from_file();//从文件中读取信息函数头 
-void find_and_modify(struct Record *head);//修改成绩函数头 
-void show(struct Record *head);//展示函数头 
-void free_list(struct Record *head);//清除内存 
-void statistic_analysis(struct Record *head); //统计并分析信息 函数头 
-void bubbleSort_records(struct Record *head);//把数据进行排序 
-void find_fail_student(struct Record *head);//查找不合格的学员，并打印 
-void search_by_name(struct Record *head);//按名字查找学员记录 
-void menu(struct Record *head);//菜单函数头 
+void safe_input_int(int *p,int min,int max);
+struct Record * create_linkList();
+struct Record *batch_add_node(struct Record *head);
+struct Record * batch_delete_node(struct Record *head);
+void find_and_modify(struct Record *head);
+void inquire_info(struct Record *head);
+void show(struct Record *head);
+void save_to_file(struct Record *head);
+struct Record* load_from_file();
+void bubble_resort_score(struct Record *head);
+void statistic_analysis(struct Record *head);
+void free_linkList(struct Record *head);
+void menu(struct Record *head);
